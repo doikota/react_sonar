61a49d4672ab5871ea4bd03d1585d78d1f005e90
doikota <doikota@gmail.com>
2024-06-30 18:18:47 +0900

    Modified eslint.config.mjs to more understandable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,12 @@ import pluginJs from '@eslint/js';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,jsx}'] },
-  { settings: { react: { version: 'detect' } } },
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  { languageOptions: { globals: globals.browser } },
-  { rules: { 'react/jsx-boolean-value': ['error', 'always'] } },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    settings: { react: { version: 'detect' } },
+    languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } }, globals: globals.browser },
+    rules: { 'react/jsx-boolean-value': ['error', 'always'] },
+  },
   pluginJs.configs.recommended,
   pluginReactConfig,
 ];
